f9616ddc982483b7b646d8b7aa345071bbb402b1
TBXark <tbxark@outlook.com>
2019-10-10 13:52:02 +0800

    Initial Commit

---
+++ PasswordX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasswordX
+//
+//  Created by TBXark on 2019/10/10.
+//  Copyright © 2019 TBXark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
